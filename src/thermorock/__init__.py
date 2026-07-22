740033870bab6d603b48a1ea45af59d6cba9e1b3
Titus Joseph <shekonuguza@gmail.com>
2026-07-22 14:49:10 +0200

    Improve package public API

--- src/thermorock/__init__.py
+++ src/thermorock/__init__.py
@@ -1,6 +1,29 @@
-# thermorock package
-from .diffusivity import thermal_diffusivity
-from .validation import validate_positive
+"""
+ThermoRock
+
+A Python package for thermal rock properties and geothermal calculations.
+"""
+
 from .database import get_rock_properties
+from .diffusivity import thermal_diffusivity
+from .geothermal import (
+    geothermal_gradient,
+    temperature_at_depth,
+    heat_flow,
+)
 from .range_validation import validate_property_range
-from .geothermal import geothermal_gradient
+from .validation import (
+    validate_positive,
+    validate_positive_inputs,
+)
+
+__all__ = [
+    "get_rock_properties",
+    "thermal_diffusivity",
+    "geothermal_gradient",
+    "temperature_at_depth",
+    "heat_flow",
+    "validate_positive",
+    "validate_positive_inputs",
+    "validate_property_range",
+]
